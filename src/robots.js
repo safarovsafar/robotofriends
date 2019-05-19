57f50ba52ed09d00b068c078126aa756ee6abb32
safarovsafar <safarov7safar@gmail.com>
2019-05-19 23:58:34 +0500

    shot

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Pushkin',
+        username: 'Alexander',
+        email: 'alexandersergeevichpushkin@gamil.com'
+    },
+    {
+        id: 2,
+        name: 'Lermontov',
+        username: 'Mikhail',
+        email: 'mikhailyureevichLermontov@gamil.com'
+    },
+    {
+        id: 3,
+        name: 'Lev',
+        username: 'Tolstoy',
+        email: 'levnikolaeevichTolstoy@gamil.com'
+    },
+    {
+        id: 4,
+        name: 'Fyodor',
+        username: 'Dostoyevskiy',
+        email: 'fyodormikhailovichDostoevsky@gamil.com'
+    },
+    {
+        id: 5,
+        name: 'Nikolai',
+        username: 'Gogol',
+        email: 'nikolaivasilievichGogo@gamil.com'
+    },
+    {
+        id: 6,
+        name: 'Mikhail',
+        username: 'Bulgakov',
+        email: 'nikhailafanasyevichBulgakov@gamil.com'
+    },
+    {
+        id: 7,
+        name: 'Alexander ',
+        username: 'Ostrovsky',
+        email: 'alexandernikolayevichOstrovsky@gamil.com'
+    },
+    {
+        id: 8,
+        name: 'Anton ',
+        username: 'Chekhov',
+        email: 'antonpavlovichChekhov@gamil.com'
+    },
+    {
+        id: 9,
+        name: 'Nikolay',
+        username: 'Nekrasov',
+        email: 'nikolayalexeyevichNekrasov@gamil.com'
+    },
+    {
+        id: 10,
+        name: 'Maxim',
+        username: 'Gorky',
+        email: 'alexeimaximovichPeshkov@gamil.com'
+    },
+
+]
